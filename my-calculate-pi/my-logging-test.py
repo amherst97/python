3f72924b534bcdcb03156af0249fab89e2b8459d
amherst97 <dansong.chen@gmail.com>
2020-02-15 11:37:01 -0500

    test basicConfig

--- my-calculate-pi/my-logging-test.py
+++ my-calculate-pi/my-logging-test.py
@@ -1,5 +1,7 @@
 import logging
 
+logging.basicConfig(level=logging.DEBUG)
+
 logging.debug("This is a debug message")
 logging.info("This is a info message")
 logging.warning("This is a warning message")
