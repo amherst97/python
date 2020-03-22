d441c37ca6b4fc5327cb8973cd694a25c27e07cd
Dansong Chen <dansong.chen@gmail.com>
2020-03-22 10:28:59 -0400

    Add logging module

--- my-test-structure/application.py
+++ my-test-structure/application.py
@@ -4,10 +4,13 @@ Test docstring
 """
 
 import sys
+import logging
 from urllib.request import urlopen
 from package1 import module1
 from package2 import module2
 from package2.package3 import module3
+
+log = logging.getLogger(__name__)
 
 
 def fetch_words():
@@ -21,9 +24,14 @@ def fetch_words():
         for word in story_words:
             print(word)
 
+        log.info("Done fetch words")
+
 
 if __name__ == '__main__':
+    log.info("Starting the application")
     print(sys.path)
     module1.function1()
     module2.function2()
+
+    #
     module3.function3()
