80f35bda4347e39039da6bedc11ac713ed8af764
Dansong Chen <dansong.chen@gmail.com>
2022-04-25 21:55:02 -0400

    Update logic per lina

--- bank-data-recon/recon.py
+++ bank-data-recon/recon.py
@@ -9,9 +9,8 @@ old_list = []
 new_list = []
 
 
-def print_list_to_file(row_list):
-    output_file = 'output.txt'
-    with open(output_file, 'wt') as f:
+def print_list_to_file(row_list, filename):
+    with open(filename, 'wt') as f:
         f.write('\n'.join(row_list))
 
 
@@ -51,31 +50,41 @@ def compare_old_with_new():
 
 
 def compare_new_with_old():
-    new_unique_schema = []
+    new_unique_result = []
+    exact_match_result = []
+
     for new_row in new_list:
         # retrieve database name + table name
         found = False
         new_db_name = new_row['NEWSQL']
         new_table_name = new_row['DATASOURCETABLE']
         old_db_name = new_dbname_mapping[new_db_name]
-        new_table_name = new_table_name.replace('.', '_')
+        old_table_name = new_table_name.replace('.', '_')
 
-        new_table_datalength = re.findall('[0-9]]', new_row['DATALENGTH'])
+
         for old_row in old_list:
-            old_table_datalength = re.findall('[0-9]]', old_row['DATALENGTH'])
+            print(old_row)
+            old_table_datalength = re.findall(r'\d+|$', old_row['DATALENGTH'])[0]
+            new_table_datalength = re.findall(r'\d+|$', new_row['DATALENGTH'])[0]
+
             if old_db_name == old_row['SUBJECTAREA'] and old_table_name == old_row['DATASOURCETABLE']:
                 if old_row['DATASOURCEFIELD'] == new_row['DATASOURCEFIELD']:
                     if(new_row['M/S'] == "S"):
-
-                        old_row['DATALENGTH'] = max(new_table_datalength, old_table_datalength)
-                        result = f"{old_row['DATALENGTH']}"
+                        temp = max(new_table_datalength, old_table_datalength)
+                        result1 = f"{old_db_name},{old_table_name},{old_row['DATASOURCEFIELD']}, {old_row['DATATYPE']},{temp}"
+                        exact_match_result.append(result1)
+                    if(new_row['M/S'] == "M" and new_row['DATATYPE']!='decimal'):
+                        temp1 = max((3*int(new_table_datalength))+3, int(old_table_datalength))
+                        result2 = f"{old_db_name},{old_table_name},{old_row['DATASOURCEFIELD']}, {old_row['DATATYPE']},{temp1}"
+                        exact_match_result.append(result2)
                     found = True
                     break
         if not found:
             result = f"{new_db_name},{new_table_name},{new_row['DATASOURCEFIELD']} {new_row['DATATYPE']} {new_row['M/S']} {new_row['DATALENGTH']}"
             print(result)
-            new_unique_schema.append(result)
-    return new_unique_schema
+            new_unique_result.append(result)
+    return new_unique_result, exact_match_result
+
 
 if __name__ == '__main__':
     print("start recon process")
@@ -83,4 +92,9 @@ if __name__ == '__main__':
     new_file_name = 'NewTables.txt'
     read_files(old_file_name, new_file_name)
     # compare_old_with_new()
-    print_list_to_file(compare_new_with_old())
+
+    unique_match, exact_match = compare_new_with_old()
+    print(f"UNIQUE FILE")
+    print_list_to_file(unique_match, "unique.txt")
+    print(f"EXACT FILE")
+    print_list_to_file(exact_match, "exact.txt")
